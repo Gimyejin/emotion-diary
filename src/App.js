15543a27585c4a35eee7b84f22757f09a4edabaa
yejinGim <yjgim@wesome.co.kr>
2022-10-11 12:53:28 +0900

    React Router Dom의 기능 사용하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
+import RouteTest from './components/RouteTest';
 
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
           <Route path='/edit' element={<Edit />} />
-          <Route path='/diary' element={<Diary />} />
+          <Route path='/diary/:id' element={<Diary />} />
         </Routes>
+        <RouteTest />
       </div>
     </BrowserRouter>
   );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [serchParams, setSearcgParams] = useSearchParams();
+    const id = serchParams.get('id');
+    const mode = serchParams.get('mode');
+
+    console.log("id", id, "mode", mode)
     return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일기 수정페이지 입니다</p>
+            <button onClick={() => setSearcgParams({ who: "winterload" })}>qs 바꾸기</button>
+            <button onClick={() => { navigate("/home") }}>Home으로 가기</button>
+            <button onClick={() => { navigate(-1) }}>뒤로가기</button>
         </div>
     );
 };
